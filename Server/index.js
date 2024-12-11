0bd405cb63b728ee4a3eb237ebcb2fe171133f3e
JohnRendell <catuiza.johnrendell@gmail.com>
2024-12-11 14:30:17 +0800

    Added 404 folders, tailwind library and other web components

--- Server/index.js
+++ Server/index.js
@@ -5,13 +5,20 @@ const path = require('path');
 //get the env keys
 require('dotenv').config({ path: path.resolve(__dirname, '../keys.env') });
 
-//serve the static files
-app.get(express.static(path.join(__dirname, '../Public')));
+//serve the static folders
+app.use(express.static(path.join(__dirname, '../Public')));
+app.use(express.static(path.join(__dirname, '../Assets')));
+app.use(express.static(path.join(__dirname, '../404pages')));
 
 //get the main html file
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../Public', 'index.html'));
-})
+});
+
+//for 404 pages
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../404pages', 'index.html'));
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{

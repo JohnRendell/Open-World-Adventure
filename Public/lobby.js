b6b4488803660d68746e39b862b39d11c00b66c6
JohnRendell <catuiza.johnrendell@gmail.com>
2024-12-19 11:29:05 +0800

    Add NPC idle animation and some stuff

--- Public/lobby.js
+++ Public/lobby.js
@@ -16,8 +16,17 @@ const gameLobby = new Phaser.Scene("Game Lobby");
 gameLobby.preload = function() {
     this.load.image('lobby', '/ImageComponents/Lobby/Lobby Hall.png');
     this.load.image('guest_sprite', '/ImageComponents/Lobby/guest player.png');
-    this.load.image('spawner_pod', '/ImageComponents/Lobby/Spawner Pod.png');
-    this.load.spritesheet('spawn_smoke', '/ImageComponents/Lobby/Sprite Sheets/spawn smoke sprite sheet.png', {
+    this.load.image('spawner_pod', '/ImageComponents/Objects/Spawner Pod.png');
+    this.load.image('table', '/ImageComponents/Objects/Long table.png')
+
+    //NPC
+    this.load.spritesheet('Rupert_NPC', '/ImageComponents/Sprite Sheets/Rupert Idle sprite sheet.png', {
+        frameWidth: 1600 / 5,
+        frameHeight: 800 / 1
+    });
+
+    //sprite sheets
+    this.load.spritesheet('spawn_smoke', '/ImageComponents/Sprite Sheets/spawn smoke sprite sheet.png', {
         frameWidth: 4800 / 5,
         frameHeight: 1600 / 2
     });
@@ -73,8 +82,8 @@ gameLobby.create = function() {
     //add the spawner pod
     this.spawner = this.physics.add.staticSprite((canvasSize.width / 2) - 50, canvasSize.height / 2, 'spawner_pod').setOrigin(0.5);
     this.spawner.setDisplaySize(50, 70);
-    this.spawner.body.setSize(50, 70, true);
-    this.spawner.body.setOffset(200, 365);
+    this.spawner.body.setSize(50, 20, true);
+    this.spawner.body.setOffset(200, 420);
 
     //spawner Label
     this.spawnerLabel = this.add.text((canvasSize.width / 2) - 50, (canvasSize.height / 2) - 50, "Spawner Pod", {
@@ -85,6 +94,60 @@ gameLobby.create = function() {
 
     //collider for spawner
     this.physics.add.collider(this.playerContainer, this.spawner);
+
+   var npc = (npcKey, imageLabel, objectLabel, NPCBodyOffsetX, NPCBodyOffsetY, NPCposX, NPCposY)=>{
+        //add the NPC
+        const npcObj = this.physics.add.staticSprite(NPCposX, NPCposY, imageLabel).setOrigin(0.5);
+        npcObj.setDisplaySize(40, 70);
+        npcObj.body.setSize(40, 20, true);
+        npcObj.body.setOffset(NPCBodyOffsetX, NPCBodyOffsetY);
+
+        //NPC Label
+        this.NPCLabel = this.add.text(NPCposX, (NPCposY - 100) + 50, objectLabel, {
+            font: "16px 'Pixelify Sans",
+            fill: "#ffffff",
+            align: "center"
+        }).setOrigin(0.5);
+
+        const instructionText = this.add.text(NPCposX, (NPCposY - 100) + 20, "Click to talk", {
+            font: "16px 'Pixelify Sans",
+            fill: "#ffffff",
+            align: "center"
+        }).setOrigin(0.5);
+        instructionText.setVisible(false);
+
+        //animation idle for npc
+        this.anims.create({
+            key: npcKey,
+            frames: this.anims.generateFrameNumbers(imageLabel, { start: 0, end: 1 }),
+            frameRate: 4,
+            repeat: -1
+        });
+
+        npcObj.play(npcKey);
+
+        //trigger for this npc, on enter
+        this.physics.add.overlap(this.playerContainer, npcObj, () => {
+            instructionText.setVisible(true);
+        });
+
+        //collider for npc
+        this.physics.add.collider(this.playerContainer, npcObj);
+
+        this[npcKey] = npcObj;
+        this[`${npcKey}Text`] = instructionText;
+   }
+
+   npc.call(this, 'rupert', 'Rupert_NPC', 'Rupert (NPC)', 140, 420, (canvasSize.width / 2) + 200, (canvasSize.height / 2) + 100);
+
+   //table beside Rupert
+   this.rupertTable = this.physics.add.staticSprite((canvasSize.width / 2) + 150, (canvasSize.height / 2) + 120, 'table').setOrigin(0.5);
+    this.rupertTable.setDisplaySize(40, 50);
+    this.rupertTable.body.setSize(40, 55, true);
+    this.rupertTable.body.setOffset(300, 130);
+
+    //collider for table
+    this.physics.add.collider(this.playerContainer, this.rupertTable);
 
     //Follow player
     this.playerContainer.body.setCollideWorldBounds(true);
@@ -121,5 +184,10 @@ gameLobby.update = function() {
     }
     if(this.cursors.down.isDown || this.S.isDown){
         this.playerContainer.body.setVelocityY(speed);
+    }
+
+    // Check if player is outside the overlap
+    if (!Phaser.Geom.Intersects.RectangleToRectangle(this.playerContainer.getBounds(), this.rupert.getBounds())) {
+        this.rupertText.setVisible(false); // Hide text if player leaves the NPC
     }
 }

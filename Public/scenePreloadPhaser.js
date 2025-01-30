19da00a5f4a24b8d6be172899e31a2ff83269cf1
JohnRendell <catuiza.johnrendell@gmail.com>
2025-01-30 19:44:42 +0800

    checked the sprite sheet naming

--- Public/scenePreloadPhaser.js
+++ Public/scenePreloadPhaser.js
@@ -7,7 +7,7 @@ function loadAssets(scene) {
     scene.load.image('lobby', '/ImageComponents/Lobby/Lobby Island.png');
     scene.load.image('rock', '/ImageComponents/Objects/Rock.png');
     scene.load.image('tree', '/ImageComponents/Objects/Tree.png');
-    scene.load.spritesheet('spawner', '/ImageComponents/Sprite Sheets/Spawner Sprite Sheet.png', {
+    scene.load.spritesheet('spawner', '/ImageComponents/Sprite Sheets/Spawner effect Sprite Sheet.png', {
         frameWidth: 5280 / 5,
         frameHeight: 800 / 1
     });
